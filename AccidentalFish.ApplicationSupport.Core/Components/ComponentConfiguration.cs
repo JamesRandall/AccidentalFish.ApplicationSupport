9871094961aa80c87bb9f77e99efaeedd21ec433
James Randall <james@accidentalfish.com>
2013-11-21 18:58:30 +0000

    Small piece of work to support component configuration

---
+++ AccidentalFish.ApplicationSupport.Core/Components/ComponentConfiguration.cs
@@ -0,0 +1,11 @@
+﻿namespace AccidentalFish.ApplicationSupport.Core.Components
+{
+    public class ComponentConfiguration
+    {
+        public IComponentIdentity ComponentIdentity { get; set; }
+
+        public int MinimumInstance { get; set; }
+
+        public IComponentIdentity RetryPolicyIdentity { get; set; }
+    }
+}
---
+++ AccidentalFish.ApplicationSupport.Core/Components/IComponentHostConfigurationProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AccidentalFish.ApplicationSupport.Core.Components
+{
+    public interface IComponentHostConfigurationProvider
+    {
+        Task<IEnumerable<ComponentConfiguration>> GetConfiguration();
+    }
+}
