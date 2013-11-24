449f3703aa2b6a484337dabf5da5a383b0adc611
James Randall <james@accidentalfish.com>
2013-11-24 15:28:21 +0000

    Started tests

---
+++ AccidentalFish.ApplicationSupport.Core.Tests.Unit/BackgroundProcesses/LogQueueProcessorTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AccidentalFish.ApplicationSupport.Core.Components;
+using AccidentalFish.ApplicationSupport.Core.InternalMappers;
+using AccidentalFish.ApplicationSupport.Core.Naming;
+using AccidentalFish.ApplicationSupport.Core.Policies;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace AccidentalFish.ApplicationSupport.Core.Tests.Unit.BackgroundProcesses
+{
+    [TestClass]
+    public class LogQueueProcessorTests
+    {
+        private Mock<IApplicationResourceFactory> _applicationResourceFactory;
+        private Mock<IAsynchronousBackoffPolicy> _asynchronousBackoffPolicy;
+        private Mock<IMapperFactory> _mapperFactory;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _applicationResourceFactory = new Mock<IApplicationResourceFactory>();
+            _asynchronousBackoffPolicy = new Mock<IAsynchronousBackoffPolicy>();
+            _mapperFactory = new Mock<IMapperFactory>();
+        }
+    }
+}
