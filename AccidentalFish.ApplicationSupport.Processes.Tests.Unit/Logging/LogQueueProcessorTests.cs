c9bb11229d14720ccb8dcc961bdf710ac3a7c060
James Randall <james@accidentalfish.com>
2013-12-14 09:50:28 +0000

    Started some unit tests

--- AccidentalFish.ApplicationSupport.Processes.Tests.Unit/Logging/LogQueueProcessorTests.cs
+++ AccidentalFish.ApplicationSupport.Processes.Tests.Unit/Logging/LogQueueProcessorTests.cs
@@ -1,5 +1,9 @@
-﻿using AccidentalFish.ApplicationSupport.Core.Components;
+﻿using System;
+using System.Threading;
+using AccidentalFish.ApplicationSupport.Core.Components;
+using AccidentalFish.ApplicationSupport.Core.Logging.Model;
 using AccidentalFish.ApplicationSupport.Core.Policies;
+using AccidentalFish.ApplicationSupport.Processes.Logging;
 using AccidentalFish.ApplicationSupport.Processes.Mappers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
@@ -19,6 +23,48 @@ namespace AccidentalFish.ApplicationSupport.Processes.Tests.Unit.Logging
             _applicationResourceFactory = new Mock<IApplicationResourceFactory>();
             _asynchronousBackoffPolicy = new Mock<IAsynchronousBackoffPolicy>();
             _mapperFactory = new Mock<IMapperFactory>();
+
+
+        }
+
+        [TestMethod]
+        public void ConstructorSucceeds()
+        {
+            // Act
+            new LogQueueProcessor(_applicationResourceFactory.Object, _asynchronousBackoffPolicy.Object, _mapperFactory.Object);
+        }
+
+        [TestMethod]
+        public void StartInitiaitesBackoffPolicy()
+        {
+            // Arrange
+            CancellationTokenSource source = new CancellationTokenSource();
+            LogQueueProcessor processor = new LogQueueProcessor(_applicationResourceFactory.Object, _asynchronousBackoffPolicy.Object, _mapperFactory.Object);
+
+            // Act
+            processor.Start(source.Token);
+
+            // Assert
+            _asynchronousBackoffPolicy.Verify(x => x.Execute(It.IsAny<Action<Action<bool>>>(), source.Token));
+        }
+
+        public void NullQueueItemReturnsFalseToBackoff()
+        {
+            // Arrange
+            _asynchronousBackoffPolicy.Setup(
+                x => x.Execute(It.IsAny<Action<Action<bool>>>(), It.IsAny<CancellationToken>()))
+                .Callback<Action<Action<bool>>>((action) =>
+                {
+                    action()
+                });
+            CancellationTokenSource source = new CancellationTokenSource();
+            LogQueueProcessor processor = new LogQueueProcessor(_applicationResourceFactory.Object, _asynchronousBackoffPolicy.Object, _mapperFactory.Object);
+
+            // Act
+            processor.Start(source.Token);
+
+            // Assert
+            _asynchronousBackoffPolicy.Verify(x => x.Execute(It.IsAny<Action<Action<bool>>>(), source.Token));
         }
     }
 }
--- AccidentalFish.ApplicationSupport.Processes/Properties/AssemblyInfo.cs
+++ AccidentalFish.ApplicationSupport.Processes/Properties/AssemblyInfo.cs
@@ -35,3 +35,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: InternalsVisibleTo("AccidentalFish.ApplicationSupport.Processes.Tests.Unit")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
