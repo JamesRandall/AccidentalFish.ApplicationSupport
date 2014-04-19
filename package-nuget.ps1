7ae64c3e3347cc558fa217fef009e406ca2e265a
James Randall <james@accidentalfish.com>
2014-04-19 18:05:32 +0100

    Updated packaging scripts

--- package-nuget.ps1
+++ package-nuget.ps1
@@ -4,12 +4,16 @@ rmdir nuget\tools -Recurse
 mkdir nuget\lib
 mkdir nuget\lib\net45
 cp AccidentalFish.ApplicationSupport.Core\bin\Debug\AccidentalFish.ApplicationSupport.Core.dll nuget\lib\net45
+cp AccidentalFish.ApplicationSupport.Core\bin\Debug\AccidentalFish.ApplicationSupport.Core.pdb nuget\lib\net45
 cp AccidentalFish.ApplicationSupport.Azure\bin\Debug\AccidentalFish.ApplicationSupport.Azure.dll nuget\lib\net45
+cp AccidentalFish.ApplicationSupport.Azure\bin\Debug\AccidentalFish.ApplicationSupport.Azure.pdb nuget\lib\net45
 cp AccidentalFish.ApplicationSupport.Processes\bin\Debug\AccidentalFish.ApplicationSupport.Processes.dll nuget\lib\net45
+cp AccidentalFish.ApplicationSupport.Processes\bin\Debug\AccidentalFish.ApplicationSupport.Processes.pdb nuget\lib\net45
 
 mkdir nuget\tools
 mkdir nuget\tools\net45
 cp AccidentalFish.ApplicationSupport.Powershell\bin\Debug\*.dll nuget\tools\net45
 cp package-tools-init.ps1 nuget\tools\init.ps1
 
-.\.nuget\NuGet.exe pack .\nuget\AccidentalFish.ApplicationSupport.nuspec -NoPackageAnalysis
+.\.nuget\NuGet.exe pack .\nuget\AccidentalFish.ApplicationSupport.nuspec -NoPackageAnalysis -Symbols
+cp *.nupkg ..\LocalRepos
