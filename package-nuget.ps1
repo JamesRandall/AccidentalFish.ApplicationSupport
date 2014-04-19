e625aed24b9bd4aa8ca6eac35f806dfb5767487e
James Randall <james@accidentalfish.com>
2014-04-19 10:14:13 +0100

    Added scripts for building a NuGet package

---
+++ package-nuget.ps1
@@ -0,0 +1,15 @@
+﻿rmdir nuget\lib -Recurse
+rmdir nuget\tools -Recurse
+
+mkdir nuget\lib
+mkdir nuget\lib\net45
+cp AccidentalFish.ApplicationSupport.Core\bin\Debug\AccidentalFish.ApplicationSupport.Core.dll nuget\lib\net45
+cp AccidentalFish.ApplicationSupport.Azure\bin\Debug\AccidentalFish.ApplicationSupport.Azure.dll nuget\lib\net45
+cp AccidentalFish.ApplicationSupport.Processes\bin\Debug\AccidentalFish.ApplicationSupport.Processes.dll nuget\lib\net45
+
+mkdir nuget\tools
+mkdir nuget\tools\net45
+cp AccidentalFish.ApplicationSupport.Powershell\bin\Debug\*.dll nuget\tools\net45
+cp package-tools-init.ps1 nuget\tools\init.ps1
+
+.\.nuget\NuGet.exe pack .\nuget\AccidentalFish.ApplicationSupport.nuspec -NoPackageAnalysis
