63dfe25704bab8f98f3f23da334d8b24cfc66588
James Randall <james@accidentalfish.com>
2015-09-07 21:00:57 +0100

    Added Ninject dependency resolver

---
+++ LoggerWithNinject/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using AccidentalFish.ApplicationSupport.Azure;
+using AccidentalFish.ApplicationSupport.Core;
+using AccidentalFish.ApplicationSupport.Core.Components;
+using AccidentalFish.ApplicationSupport.Core.Logging;
+using AccidentalFish.ApplicationSupport.Core.Runtime;
+using AccidentalFish.ApplicationSupport.DependencyResolver;
+using AccidentalFish.ApplicationSupport.Ninject;
+using AccidentalFish.ApplicationSupport.Processes;
+using Ninject;
+
+namespace LoggerWithNinject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKernel kernel = new StandardKernel();
+            IDependencyResolver dependencyResolver = new NinjectApplicationFrameworkDependencyResolver(kernel);
+
+            dependencyResolver
+                .UseCore()
+                .UseAzure()
+                .UseHostableProcesses();
+
+            IComponentHost componentHost = dependencyResolver.Resolve<IComponentHost>();
+            ILoggerFactory loggerFactory = dependencyResolver.Resolve<ILoggerFactory>();
+            ILogger logger = loggerFactory.CreateShortLivedLogger(new ComponentIdentity("com.accidental-fish.application-support"));
+            CancellationTokenSource source = new CancellationTokenSource();
+
+            StartComponentHost(componentHost, logger, source);
+            logger.Information("Something to log");
+            Console.ReadLine();
+            source.Cancel();
+            Thread.Sleep(500);
+        }
+
+        private static void StartComponentHost(IComponentHost componentHost, ILogger logger, CancellationTokenSource cancellationTokenSource)
+        {
+
+            componentHost.StartAsync(new StaticComponentHostConfigurationProvider(new List<ComponentConfiguration>
+            {
+                new ComponentConfiguration
+                {
+                    ComponentIdentity = HostableComponentIdentities.LogQueueProcessor,
+                    Instances = 1,
+                    RestartEvaluator = (ex, retryCount) => RestartHandler(ex, retryCount, logger, HostableComponentIdentities.LogQueueProcessor).Result
+                }
+            }), cancellationTokenSource);
+        }
+
+        private static async Task<bool> RestartHandler(Exception ex, int retryCount, ILogger logger, IComponentIdentity component)
+        {
+            try
+            {
+                bool doDelay = retryCount % 5 == 0;
+
+                if (doDelay)
+                {
+                    await
+                        logger.Warning(
+                            $"Error occurred in component {component.FullyQualifiedName}. Restarting in 30 seconds.", ex);
+                    await Task.Delay(TimeSpan.FromSeconds(30));
+                }
+                else
+                {
+                    await logger.Warning($"Error occurred in component {component.FullyQualifiedName}. Restarting immediately.", ex);
+                }
+            }
+            catch (Exception)
+            {
+                // swallow any issues
+            }
+            return true;
+        }
+    }
+}
