e75532e2ba71b9fe36f35460ab3f1acbb0db443c
James Randall <james@accidentalfish.com>
2015-09-09 19:03:16 +0100

    Adding Autofac support

---
+++ Samples/LoggerWithAutofac/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using AccidentalFish.ApplicationSupport.Autofac;
+using AccidentalFish.ApplicationSupport.Azure;
+using AccidentalFish.ApplicationSupport.Core;
+using AccidentalFish.ApplicationSupport.Core.Components;
+using AccidentalFish.ApplicationSupport.Core.Logging;
+using AccidentalFish.ApplicationSupport.Core.Runtime;
+using AccidentalFish.ApplicationSupport.Processes;
+using Autofac;
+
+namespace LoggerWithAutofac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContainerBuilder builder = new ContainerBuilder();
+            var dependencyResolver = new AutofacApplicationFrameworkDependencyResolver(builder);
+
+            dependencyResolver
+                .UseCore()
+                .UseAzure()
+                .UseHostableProcesses();
+
+            dependencyResolver.Build();
+
+            IComponentHost componentHost = dependencyResolver.Resolve<IComponentHost>();
+            ILoggerFactory loggerFactory = dependencyResolver.Resolve<ILoggerFactory>();
+            ILogger logger = loggerFactory.CreateShortLivedLogger(new ComponentIdentity("com.accidental-fish.application-support"));
+            CancellationTokenSource source = new CancellationTokenSource();
+
+            StartComponentHost(componentHost, logger, source);
+            logger.Information("Something to log");
+            Console.ReadLine();
+            source.Cancel();
+            Thread.Sleep(500);
+        }
+
+        private static void StartComponentHost(IComponentHost componentHost, ILogger logger, CancellationTokenSource cancellationTokenSource)
+        {
+
+            componentHost.StartAsync(new StaticComponentHostConfigurationProvider(new List<ComponentConfiguration>
+            {
+                new ComponentConfiguration
+                {
+                    ComponentIdentity = HostableComponentIdentities.LogQueueProcessor,
+                    Instances = 1,
+                    RestartEvaluator = (ex, retryCount) => RestartHandler(ex, retryCount, logger, HostableComponentIdentities.LogQueueProcessor).Result
+                }
+            }), cancellationTokenSource);
+        }
+
+        private static async Task<bool> RestartHandler(Exception ex, int retryCount, ILogger logger, IComponentIdentity component)
+        {
+            try
+            {
+                bool doDelay = retryCount % 5 == 0;
+
+                if (doDelay)
+                {
+                    await
+                        logger.Warning(
+                            $"Error occurred in component {component.FullyQualifiedName}. Restarting in 30 seconds.", ex);
+                    await Task.Delay(TimeSpan.FromSeconds(30));
+                }
+                else
+                {
+                    await logger.Warning($"Error occurred in component {component.FullyQualifiedName}. Restarting immediately.", ex);
+                }
+            }
+            catch (Exception)
+            {
+                // swallow any issues
+            }
+            return true;
+        }
+    }
+}
---
+++ Source/AccidentalFish.ApplicationSupport.Autofac/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AccidentalFish.ApplicationSupport.Autofac")]
+[assembly: AssemblyDescription("Autofac dependency resolver for the AccidentalFish.ApplicationSupport framework")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("AccidentalFish.ApplicationSupport.Autofac")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a44c293c-b275-4b3d-baef-b26a8e456432")]
+
